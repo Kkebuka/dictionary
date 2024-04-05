8da4512e062d5fa55bc8f038d515a7035f10cbcb
kkebuka <kkebuka@gmail.com>
2024-04-05 16:02:09 +0100

    app file

---
+++ app.js
@@ -0,0 +1,26 @@
+const dictionary = new Dictionary
+const ui = new UI
+
+const search = document.querySelector('#search');
+
+
+
+search.addEventListener('click', (e) => {
+        dictionary.getmeaning()
+        .then(data => {
+            console.log(data)
+            ui.paintUI(data)
+        })
+        .catch(err => err)
+
+        e.preventDefault()
+})
+
+
+
+
+
+
+
+
+const sound = document.querySelector('.play').addEventListener('click', () => ui.playSound())
